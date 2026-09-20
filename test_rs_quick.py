96bb0cb3310a875e0e0cd64088371605c6964ebb
Hao Yan <haoyan160@gmail.com>
2026-09-20 07:08:17 -0400

    chore(repo): remove the VLA merge's root-level status reports and setup wizards

--- test_rs_quick.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-"""Quick test of pyrealsense2 after environment fix"""
-import pyrealsense2
-print("pyrealsense2 version:", pyrealsense2.__version__)
-print("SUCCESS! Module loaded correctly")
